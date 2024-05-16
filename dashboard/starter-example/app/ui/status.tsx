688db82cc263a91c9de3ffd6111fc1402211d761
Moto Ikema <motoikama@gmail.com>
2024-05-16 17:59:29 +0900

    Fix typo

--- dashboard/starter-example/app/ui/status.tsx
+++ dashboard/starter-example/app/ui/status.tsx
@@ -10,6 +10,6 @@ export default function InvoiceStatus({ status }: { status: string }) {
           'bg-green-500 text-white': status === 'paid',
         },
       )}
-    >
+    />
     // ...
 )}
